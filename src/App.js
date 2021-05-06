f80a44cd09138762a6d1fb9bb4fa5c04fc413154
ssemenidov <23sergey03@gmail.com>
2021-05-06 19:39:52 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
-
+import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      {/* <h1>hi</h1> */}
+      <Header></Header>
+      {/* Card */}
+      {/* Buttons */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import IconButton from '@material-ui/core/IconButton';
+
+function Header() {
+    return (
+        <div className='Header'>
+            <IconButton >
+            <PersonIcon className='header__icon'/>
+            </IconButton>
+  
+            <img  src="https://cdn.worldvectorlogo.com/logos/tinder-2.svg"  alt="logo"  className='header__logo'/>
+            <IconButton>  
+                <QuestionAnswerIcon className='header__icon'/>
+                </IconButton>
+          
+     
+        </div>
+    )
+}
+
+export default Header 
