f8b1613af2f77b60bdaed97856af52a2edd57bca
ssemenidov <23sergey03@gmail.com>
2021-05-07 11:04:33 +0800

    basic buttons

--- src/components/ButtonBar.js
+++ src/components/ButtonBar.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import './ButtonBar.css'
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import CloseIcon from '@material-ui/icons/Close';
+import { green, red } from '@material-ui/core/colors';
 function ButtonBar() {
     return (
-        <div>
-            
+        <div className='buttonbar'>
+            <IconButton className='button'>
+                <CloseIcon className='button__icon' style={{ color: red[500] }}/>
+            </IconButton>
+            <IconButton className='button'>
+                <FavoriteIcon   className='button__icon'  style={{ color: green[500] }}/>
+            </IconButton>
+       
         </div>
     )
 }
